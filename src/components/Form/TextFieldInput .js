2e3985d21485d5716a469e5779e705542fd7b7b2
mahmoud <mm@gmail.com>
2023-05-14 18:17:23 +0300

    Modify style

--- src/components/Form/TextFieldInput .js
+++ src/components/Form/TextFieldInput .js
@@ -23,35 +23,36 @@ const TextFieldInput = ({
   ...rest
 }) => {
   return (
-    <Box className={styles.input} key={id}>
-      <RadioGroup value={value} onChange={onChange}>
-        {isRadio && (
+    <Box key={id}>
+      {isRadio ? (
+        <RadioGroup value={value} onChange={onChange}>
           <FormControlLabel
             className={styles.label}
             value={label}
             label={label}
             control={<Radio />}
           />
-        )}
-      </RadioGroup>
-      <TextField
-        placeholder={placeholder}
-        size="small"
-        margin="dense"
-        fullWidth={fullWidth}
-        type={type}
-        select={isSelect ? true : false}
-        value={value}
-        onChange={onChange}
-        name={rest.name}
-      >
-        {isSelect &&
-          option.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-      </TextField>
+        </RadioGroup>
+      ) : (
+        <TextField
+          placeholder={placeholder}
+          size="small"
+          margin="dense"
+          fullWidth={fullWidth}
+          type={type}
+          select={isSelect ? true : false}
+          value={value}
+          onChange={onChange}
+          name={rest.name}
+        >
+          {isSelect &&
+            option.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+        </TextField>
+      )}
     </Box>
   );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -84,7 +84,9 @@ const SignUp = () => {
                 {isAccount ? (
                   <Link to="/">{label}</Link>
                 ) : (
-                  `${label}${question}`
+                  <>
+                    {label} {question}
+                  </>
                 )}
               </Typography>
             </Box>
@@ -96,113 +98,3 @@ const SignUp = () => {
 };
 
 export default SignUp;
-
-// import React, { useState } from "react";
-// // import { makeStyles } from "@material-ui/core/styles";
-// import TextField from "@mui/material/TextField";
-// import FormControl from "@mui/material/FormControl";
-// import FormLabel from "@mui/material/FormLabel";
-// import RadioGroup from "@mui/material/RadioGroup";
-// import Radio from "@mui/material/Radio";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Select from "@mui/material/Select";
-// import MenuItem from "@mui/material/MenuItem";
-// import Button from "@mui/material/Button";
-
-// // const useStyles = makeStyles((theme) => ({
-// //   form: {
-// //     display: "flex",
-// //     flexDirection: "column",
-// //     alignItems: "center",
-// //     marginTop: theme.spacing(4),
-// //   },
-// //   input: {
-// //     margin: theme.spacing(1),
-// //   },
-// //   radioGroup: {
-// //     flexDirection: "row",
-// //   },
-// //   submitButton: {
-// //     marginTop: theme.spacing(2),
-// //   },
-// // }));
-
-// const SignupForm = () => {
-//   // useStyles();
-//   const [firstName, setFirstName] = useState("");
-//   const [lastName, setLastName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [birthday, setBirthday] = useState("");
-//   const [gender, setGender] = useState("");
-
-//   const handleGenderChange = (event) => {
-//     setGender(event.target.value);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Add code to handle form submission here
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <TextField
-//         label="First Name"
-//         value={firstName}
-//         onChange={(event) => setFirstName(event.target.value)}
-//       />
-//       <TextField
-//         label="Last Name"
-//         value={lastName}
-//         onChange={(event) => setLastName(event.target.value)}
-//       />
-//       <TextField
-//         label="Email"
-//         type="email"
-//         value={email}
-//         onChange={(event) => setEmail(event.target.value)}
-//       />
-//       <TextField
-//         label="Password"
-//         type="password"
-//         value={password}
-//         onChange={(event) => setPassword(event.target.value)}
-//       />
-//       <TextField
-//         label="Birthday"
-//         type="date"
-//         value={birthday}
-//         onChange={(event) => setBirthday(event.target.value)}
-//         InputLabelProps={{
-//           shrink: true,
-//         }}
-//       />
-//       <FormControl>
-//         <FormLabel>Gender</FormLabel>
-//         <RadioGroup value={gender} onChange={handleGenderChange}>
-//           <FormControlLabel value="male" control={<Radio />} label="Male" />
-//           <FormControlLabel value="female" control={<Radio />} label="Female" />
-//           <FormControlLabel
-//             value="nonbinary"
-//             control={<Radio />}
-//             label="Non-binary"
-//           />
-//         </RadioGroup>
-//       </FormControl>
-//       <FormControl>
-//         <FormLabel>Select your gender</FormLabel>
-//         <Select
-//           value={gender}
-//           onChange={(event) => setGender(event.target.value)}
-//         >
-//         </Select>
-//       </FormControl>
-//       <Button variant="contained" color="primary" type="submit">
-//         Sign Up
-//       </Button>
-//     </form>
-//   );
-// };
-
-// export default SignupForm;
