aaf066ae16f1b684bda306104a7553f371e50754
mahmoud <mm@gmail.com>
2023-05-24 12:52:33 +0300

    Create MainPost Component

---
+++ src/components/pages/Home/posts/MainPosts.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import CreatePost from "./CreatePost";
+import Post from "./Post";
+
+const MainPostForm = () => {
+  return (
+    <>
+      <CreatePost />
+      <Post />
+    </>
+  );
+};
+
+export default MainPostForm;
