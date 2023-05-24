562fd1964270672232b0d1bdb2bd4cc9aad119c0
mahmoud <mm@gmail.com>
2023-05-24 12:50:45 +0300

    Styled post component

---
+++ src/components/pages/Home/posts/PostStyle.js
@@ -0,0 +1,96 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Input,
+  TextareaAutosize,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const CustomMainPostFormBox = styled(Box)(({}) => ({
+  width: "745px",
+  overflowY: "scroll",
+  scrollbarColor: "#BCC0C4",
+  "&::-webkit-scrollbar": {
+    width: "5px",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: "#BCC0C4",
+    borderRadius: "20px",
+  },
+}));
+
+export const CustomTextareaAutosize = styled(TextareaAutosize)(({ theme }) => ({
+  resize: "none",
+  width: "98%",
+  borderRadius: "50px",
+  backgroundColor: "transparent",
+  border: "none",
+  padding: "8px 12px",
+  "&:focus": {
+    outline: "none",
+  },
+  marginLeft: "8px",
+}));
+
+export const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  width: "100%",
+  color: "#65676b",
+  fontSize: "15px",
+  borderRadius: "8px",
+  marginTop: "10px",
+  "&:hover": {
+    color: "#0573e7",
+    backgroundColor: "#f7f8fa",
+    backgroundColor: "#f0f2f5",
+  },
+}));
+
+export const CustomDialogActionsButton = styled(Button)(({}) => ({
+  width: "100%",
+  color: "#fff",
+  backgroundColor: "#1976d2",
+  "&:hover": {
+    backgroundColor: "#1976d2",
+  },
+}));
+
+export const CustomIconButtonImgUpload = styled(IconButton)(({}) => ({
+  width: "100%",
+  height: "500px",
+  backgroundColor: "#f7f8fa",
+  padding: "10px",
+  outline: "1px solid #ddd",
+  outlineOffset: "5px",
+  borderRadius: "8px",
+  marginTop: "32px",
+}));
+
+export const CustomIconButtonReaction = styled(IconButton)(({}) => ({
+  width: "calc(100% / 3)",
+  borderRadius: "8px",
+}));
+
+export const CustomAvatar = styled(Avatar)(({}) => ({
+  width: "30px",
+  height: "30px",
+  marginRight: "10px",
+  cursor: "pointer",
+}));
+
+export const CustomInput = styled(Input)(({}) => ({
+  width: "100%",
+  backgroundColor: "#f0f2f5",
+  borderRadius: "50px",
+  paddingLeft: "10px",
+  "&::before": {
+    border: "none",
+  },
+  "&:hover:before": {
+    border: "none",
+  },
+  "&::after": {
+    border: "none",
+  },
+}));
