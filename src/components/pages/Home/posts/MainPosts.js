ad0e85821212d4f7bb3e075eea5586ed752d2dd7
mahmoud <mm@gmail.com>
2023-05-25 02:20:48 +0300

    Add context provider

--- src/components/pages/Home/posts/MainPosts.js
+++ src/components/pages/Home/posts/MainPosts.js
@@ -1,13 +1,15 @@
-import { Box } from "@mui/material";
 import CreatePost from "./CreatePost";
 import Post from "./Post";
+import ImageContext from "../../../../context/ImageContext";
+import { useState } from "react";
 
 const MainPostForm = () => {
+  const [imageUrls, setImageUrls] = useState([]);
   return (
-    <>
+    <ImageContext.Provider value={{ imageUrls, setImageUrls }}>
       <CreatePost />
       <Post />
-    </>
+    </ImageContext.Provider>
   );
 };
 
