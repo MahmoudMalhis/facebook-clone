1e2b46dccec41989ebdd8b88c377da80bb3daa63
mahmoud <mm@gmail.com>
2023-05-26 23:52:34 +0300

    Add delete post

--- src/components/pages/Home/posts/Post/PostHeader.js
+++ src/components/pages/Home/posts/Post/PostHeader.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import {
   Avatar,
   Box,
@@ -10,9 +10,14 @@ import {
 } from "@mui/material";
 import { CustomLinearScaleIcon } from "../PostStyle";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { AuthContext } from "../../../../../context/AuthContext";
+import { deleteDoc, doc } from "firebase/firestore";
+import { firestore } from "../../../../firebase";
 
 const PostHeader = ({ post }) => {
   const [anchorPost, setAnchorPost] = useState(null);
+  const userFullName = useContext(AuthContext);
 
   const handlePostMenu = (event) => {
     setAnchorPost(event.currentTarget);
@@ -22,12 +27,21 @@ const PostHeader = ({ post }) => {
     setAnchorPost(null);
   };
 
+  const handleDelete = async (postId) => {
+    try {
+      const postRef = doc(firestore, "posts", postId);
+      await deleteDoc(postRef);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between">
       <Box display="flex" alignItems="center">
         <Avatar />
         <Box marginLeft="10px">
-          <Typography fontWeight="700">Mahmoud</Typography>
+          <Typography fontWeight="700">{userFullName}</Typography>
           <Typography fontSize="12px" color="#999">
             {post.createdAt}
           </Typography>
@@ -56,6 +70,10 @@ const PostHeader = ({ post }) => {
         <MenuItem onClick={handleClosePostMenu}>
           <BookmarkIcon />
           <Typography textAlign="center">Saved</Typography>
+        </MenuItem>
+        <MenuItem onClick={() => handleDelete(post.id)}>
+          <DeleteIcon />
+          <Typography textAlign="center">Delete</Typography>
         </MenuItem>
       </Menu>
     </Box>
