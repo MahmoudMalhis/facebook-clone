617f15f76bd771d4b549c3de44c088ac25d15c0c
mahmoud <mm@gmail.com>
2023-05-28 16:39:54 +0300

    Create UserInfo component

---
+++ src/components/pages/Profile/UserInfo.js
@@ -0,0 +1,45 @@
+import { Fragment, useContext } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import { List, ListItem, ListItemText } from "@mui/material";
+import { CustomListItemTextUserInfo, CustomListUserInfo } from "./StyleProfile";
+
+const UserInfo = () => {
+  const userData = useContext(AuthContext);
+
+  const Info = [
+    {
+      id: 1,
+      label: "Gender:",
+      value: userData.gender,
+    },
+    {
+      id: 1,
+      label: "Barth day:",
+      value: `${userData.selectedYear} - ${userData.selectedMonth}- ${userData.selectedDay}`,
+    },
+    {
+      id: 1,
+      label: "Email:",
+      value: userData.email,
+    },
+    {
+      id: 1,
+      label: "Age:",
+      value: new Date().getFullYear() - userData.selectedYear,
+    },
+  ];
+
+  return (
+    <CustomListUserInfo>
+      {Info.map(({ id, label, value }) => {
+        return (
+          <ListItem key={id}>
+            <CustomListItemTextUserInfo primary={label} secondary={value} />
+          </ListItem>
+        );
+      })}
+    </CustomListUserInfo>
+  );
+};
+
+export default UserInfo;
