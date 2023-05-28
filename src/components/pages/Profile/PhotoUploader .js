ba5ca5f5c7644214c7e4a8f882b5eab16b96d3d6
mahmoud <mm@gmail.com>
2023-05-28 13:36:27 +0300

    Create photoUploader component

---
+++ src/components/pages/Profile/PhotoUploader .js
@@ -0,0 +1,30 @@
+import { Input } from "@mui/material";
+import FilterIcon from "@mui/icons-material/Filter";
+import { CustomIconButtonImgUpload } from "../Home/posts/PostStyle";
+
+const PhotoUploader = ({ onChange, selectedImage }) => (
+  <>
+    <Input
+      type="file"
+      accept="image/*"
+      onChange={onChange}
+      style={{ display: "none" }}
+      id="upload-button"
+    />
+    <label htmlFor="upload-button">
+      <CustomIconButtonImgUpload component="span">
+        {selectedImage ? (
+          <img
+            style={{ width: "auto", height: "auto", overflowY: "scroll" }}
+            src={selectedImage}
+            alt="Uploaded"
+          />
+        ) : (
+          <FilterIcon />
+        )}
+      </CustomIconButtonImgUpload>
+    </label>
+  </>
+);
+
+export default PhotoUploader;
