64fee8a3ee9ad5b5d6337fabd332e5c31ffe80d8
mahmoud <mm@gmail.com>
2023-05-30 19:31:29 +0300

    responsive

--- src/components/pages/SavePost/Save.js
+++ src/components/pages/SavePost/Save.js
@@ -2,13 +2,17 @@ import React from "react";
 import LeftSide from "../Home/LeftSide/LeftSide";
 import { CustomBox } from "../Home/HomeStyled";
 import PostSaved from "./PostSaved";
+import { Box } from "@mui/material";
+import { Container, SaveBox } from "./SaveStyle";
 
 const Save = () => {
   return (
-    <CustomBox>
-      <LeftSide />
-      <PostSaved />
-    </CustomBox>
+    <SaveBox>
+      <Container>
+        <LeftSide />
+        <PostSaved />
+      </Container>
+    </SaveBox>
   );
 };
 
